f7f570f916d5aaabaca0b196e60d2256a13884c8
Aaron Soukaphay <aaronsoukaphay@gmail.com>
2023-11-27 23:48:08 -0800

    refactored projects section to make code more reusable, made about section two columns

--- my-website/src/About.tsx
+++ my-website/src/About.tsx
@@ -1,17 +1,4 @@
 export default function About() {
-  // const skills = [
-  //   "TypeScript",
-  //   "JavaScript",
-  //   "React.js",
-  //   "HTML5",
-  //   "CSS3",
-  //   "Node.js",
-  //   "Express.js",
-  //   "PostgreSQL",
-  //   "Git",
-  //   "GitHub",
-  // ];
-
   const skills = [
     {
       name: "TypeScript",
@@ -73,33 +60,30 @@ export default function About() {
 
   return (
     <div className="h-screen flex items-center">
-      <div>
-        <div className="text-center text-2xl">About Me</div>
-        <div className="text-xl">
-          Hello! My journey began in the healthcare industry, but my true
-          passion lies in the dynamic world of web development. Holding a degree
-          in science has instilled in me a strong foundation in research,
-          critical thinking, and problem-solving. This background, combined with
-          my background in web development, brings a unique perspective to my
-          work.
+      <div className="flex gap-5">
+        <div className="basis-1/2">
+          <div className="text-2xl pb-4">Get to know me!</div>
+          <div className="text-xl">
+            Hello! My journey began in the healthcare industry, but my true
+            passion lies in the dynamic world of web development. Holding a
+            degree in science has instilled in me a strong foundation in
+            research, critical thinking, and problem-solving. This background,
+            combined with my background in web development, brings a unique
+            perspective to my work.
+          </div>
         </div>
-        <div className="text-center text-2xl">My Skills</div>
-        <div className="text-xl">
-          I recently completed an intensive and accelerated web development
-          program. This program equipped me with a diverse set of valuable
-          skills, including JavaScript ES5/ES6, TypeScript, React.js, Node.js,
-          Express, HTML5, and CSS3, enabling me to hit the ground running in the
-          fast-paced world of web development.
-        </div>
-        <div className="flex flex-wrap justify-evenly py-3 gap-1">
-          {skills.map((skill) => (
-            <div className="bg-white text-black rounded px-4 py-2 xl:basis-1/6 md:basis-1/5 sm:basis-1/2">
-              <div className="flex justify-center">
-                <img src={skill.img} />
+        <div className="basis-1/2">
+          <div className="text-2xl pb-4">My Skills</div>
+          <div className="flex flex-wrap gap-1">
+            {skills.map((skill) => (
+              <div className="bg-white text-black rounded px-4 py-2 xl:basis-1/6 md:basis-1/5 sm:basis-1/2">
+                <div className="flex justify-center">
+                  <img src={skill.img} />
+                </div>
+                <h1 className="text-center">{skill.name}</h1>
               </div>
-              <h1 className="text-center">{skill.name}</h1>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       </div>
     </div>
--- my-website/src/Projects.tsx
+++ my-website/src/Projects.tsx
@@ -1,45 +1,63 @@
 export default function Projects() {
-  const ajax = ["JavaScript", "HTML", "CSS", "Jikan API"];
-  const final = [
-    "TypeScript",
-    "React.js",
-    "PostgreSQL",
-    "Express.js",
-    "Node.js",
-    "Bootstrap",
+  const projects = [
+    {
+      title: "Anime Experts",
+      description:
+        "A dynamic web application for anime enthusiasts who want to explore anime and manage their watchlist.",
+      technologies: ["JavaScript", "HTML", "CSS", "Jikan API"],
+      image: "/images/anime-experts.png",
+      live: "https://aaronsoukaphay.github.io/anime-experts/",
+      repo: "https://github.com/aaronsoukaphay/anime-experts",
+    },
+    {
+      title: "Touchdown Threads",
+      description:
+        "A full stack e-commerce web application for NFL enthusiasts who want to shop for football apparel.",
+      technologies: [
+        "TypeScript",
+        "React.js",
+        "PostgreSQL",
+        "Express.js",
+        "Node.js",
+        "Bootstrap",
+      ],
+      image: "/images/touchdown-threads.png",
+      live: "http://final-project-dev.us-west-1.elasticbeanstalk.com/",
+      repo: "https://github.com/aaronsoukaphay/touchdown-threads",
+    },
   ];
 
   return (
-    <div className="h-screen flex items-center">
-      <div>
-        <h1 className="text-center text-2xl">My Projects</h1>
-        <p className="text-center mb-5">
-          Here are some projects I've worked on.
-        </p>
-        <div className="flex justify-between gap-6">
-          <div className="bg-medgrey rounded">
-            <img src="/images/anime-experts.png" className="rounded-t" />
+    <div>
+      <h1 className="text-center text-2xl">My Projects</h1>
+      <p className="text-center mb-5">Here are some projects I've worked on.</p>
+      <div className="flex flex-wrap justify-center gap-3">
+        {projects.map((project) => (
+          <div className="bg-medgrey rounded basis-2/5">
+            <div className="flex justify-center bg-contain">
+              <img
+                src={project.image}
+                className="rounded-t object-cover h-96 w-full"
+              />
+            </div>
             <div className="p-5">
-              <div className="text-2xl py-3">Anime Experts</div>
-              <div>
-                A dynamic web application for anime enthusiasts who want to
-                explore anime and manage their watchlist
-              </div>
-              <div className="flex gap-3 py-3">
-                {ajax.map((lang) => (
+              <div className="text-2xl py-3">{project.title}</div>
+              <div>{project.description}</div>
+              <div className="flex flex-wrap gap-3 py-3">
+                {project.technologies.map((lang: string) => (
                   <p className="border rounded-2xl py-1 px-2">{lang}</p>
                 ))}
               </div>
               <div className="text-center flex justify-evenly py-4">
                 <a
-                  href="https://aaronsoukaphay.github.io/anime-experts/"
+                  href={project.live}
                   target="_blank"
                   className="bg-purple rounded px-5 py-2"
                 >
                   Live
                 </a>
                 <a
-                  href="https://github.com/aaronsoukaphay/anime-experts"
+                  href={project.repo}
                   target="_blank"
                   className="bg-purple rounded px-5 py-2"
                 >
@@ -48,38 +66,7 @@ export default function Projects() {
               </div>
             </div>
           </div>
-          <div className="bg-medgrey rounded">
-            <img src="/images/touchdown-threads.png" className="rounded-t" />
-            <div className="p-5">
-              <div className="text-2xl py-3">Touchdown Threads</div>
-              <div>
-                A full stack e-commerce web application for NFL enthusiasts who
-                want to shop for football apparel.
-              </div>
-              <div className="flex gap-3 py-3">
-                {final.map((lang) => (
-                  <p className="border rounded-2xl py-1 px-2">{lang}</p>
-                ))}
-              </div>
-              <div className="text-center flex justify-evenly py-4">
-                <a
-                  href="http://final-project-dev.us-west-1.elasticbeanstalk.com/"
-                  target="_blank"
-                  className="bg-purple rounded px-5 py-2"
-                >
-                  Live
-                </a>
-                <a
-                  href="https://github.com/aaronsoukaphay/touchdown-threads"
-                  target="_blank"
-                  className="bg-purple rounded px-5 py-2"
-                >
-                  Repo
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
